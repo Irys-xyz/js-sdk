1044d6862174be55f073c2e22d242660987fb654
JesseTheRobot <jesse.cruz.wright@gmail.com>
2024-09-02 12:44:57 +0000

    feat: add eclipse to solana-web

--- packages/solana-node/src/solana.ts
+++ packages/solana-node/src/solana.ts
@@ -10,8 +10,23 @@ import {Builder, type TokenConfigTrimmed} from "@irys-network/bundler-client/bui
 }
 
 
+function getBoundSolana({name, ticker, providerUrl}: {name: string, ticker: string, providerUrl: string}) {
+    return class SolanaToken extends BaseSolanaToken {
+        constructor(config: TokenConfigTrimmed) {
+            super({name, ticker, providerUrl: config.providerUrl ?? providerUrl,
+               ...config
+             })
+        }
+    }
+}
+
 export function SolanaBundlerIrys(opts?:  { finality?: Finality; disablePriorityFees?: boolean } ) {
     // return a builder
     return new Builder(SolanaToken).withTokenOptions(opts)
 }
 export default SolanaBundlerIrys
+
+export function EclipseBundlerIrys(opts?:  { finality?: Finality; disablePriorityFees?: boolean } ) {
+    return new Builder(getBoundSolana({name: "eclipse", ticker: "ETH", providerUrl:  "https://mainnetbeta-rpc.eclipse.xyz"}))
+    .withTokenOptions(opts)
+}
--- packages/solana-web/src/solana.ts
+++ packages/solana-web/src/solana.ts
@@ -9,9 +9,25 @@ import {Builder, type TokenConfigTrimmed} from "@irys-network/web-bundler-client
     }
 }
 
+function getBoundSolana({name, ticker, providerUrl}: {name: string, ticker: string, providerUrl: string}) {
+    return class SolanaToken extends BaseSolanaToken {
+        constructor(config: TokenConfigTrimmed) {
+            super({name, ticker, providerUrl: config.providerUrl ?? providerUrl,
+               ...config
+             })
+        }
+    }
+}
+
+
 
 export function SolanaBundlerWebIrys(opts?:  { finality?: Finality; disablePriorityFees?: boolean } ) {
     // return a builder
     return new Builder(SolanaToken).withTokenOptions(opts)
 }
 export default SolanaBundlerWebIrys
+
+export function EclipseBundlerWebIrys(opts?:  { finality?: Finality; disablePriorityFees?: boolean } ) {
+    return new Builder(getBoundSolana({name: "eclipse", ticker: "ETH", providerUrl:  "https://mainnetbeta-rpc.eclipse.xyz"}))
+    .withTokenOptions(opts)
+}
